5834cca82f77278a493e50ddb02cb2f29a4ad185
Neil Aldea <njaldea@gmail.com>
2024-04-15 15:09:52 -0400

    [consume] updated to latest working example

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -1,13 +1,22 @@
-set(ENABLE_TEST OFF CACHE BOOL "[0 | OFF - 1 | ON]: build tests?")
-
-function(add_test_subdirectory DIRECTORY)
-    if(ENABLE_TEST)
-        add_subdirectory(${DIRECTORY})
-    endif()
-endfunction()
+set(ENABLE_TEST     OFF CACHE BOOL "[0 | OFF - 1 | ON]: build tests?")
 
 if(ENABLE_TEST)
     set_property(GLOBAL PROPERTY CTEST_TARGETS_ADDED 1)
     find_package(GTest CONFIG REQUIRED)
     include(CTest)
 endif()
+
+function(add_test_executable TARGET)
+    add_executable(${TARGET} ${ARGN})
+    if(ENABLE_CLANG_TIDY AND CMAKE_CXX_CLANG_TIDY)
+        set_target_properties(${TARGET} PROPERTIES CXX_CLANG_TIDY "${CMAKE_CXX_CLANG_TIDY};-checks=-readability-function-cognitive-complexity")
+    endif()
+
+    add_test(NAME ${TARGET} COMMAND ${TARGET})
+endfunction()
+
+function(add_test_subdirectory)
+    if(ENABLE_TEST)
+        add_subdirectory(test)
+    endif()
+endfunction()
--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,6 @@
-#include <nil/cli.hpp>
+#include <nil/gate.hpp>
 
-#include <iostream>
-
-template <int V> struct Command : nil::cli::Command {
-  std::string usage() const override { return " >  <binary> [OPTIONS...]"; }
-
-  nil::cli::OptionInfo options() const override {
-    // clang-format off
-        return nil::cli::Builder()
-            .flag  ("help",   { .skey ='h', .msg = "show this help"                                        })
-            .flag  ("spawn",  { .skey ='s', .msg = "spawn"                                                 })
-            .number("thread", { .skey ='t', .msg = "number of threads"                                     })
-            .number("job",    { .skey ='j', .msg = "number of jobs"    , .fallback = 1     , .implicit = 0 })
-            .param ("param",  { .skey ='p', .msg = "default param"     , .fallback = "123"                 })
-            .params("mparam", { .skey ='m', .msg = "multiple params"                                       })
-            .build();
-    // clang-format on
-  }
-
-  int run(const nil::cli::Options &options) const override {
-    if (options.flag("help")) {
-      options.help(std::cout);
-      return 0;
-    }
-    std::cout << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__
-              << std::endl;
-    std::cout << "flag    : " << options.flag("spawn") << std::endl;
-    std::cout << "number  : " << options.number("thread") << std::endl;
-    std::cout << "number  : " << options.number("job") << std::endl;
-    std::cout << "param   : " << options.param("param") << std::endl;
-    std::cout << "params  : " << std::endl;
-    for (const auto &item : options.params("mparam")) {
-      std::cout << " -  " << item << std::endl;
-    }
+int main(int argc, const char** argv)
+{
     return 0;
-  }
-};
-
-int main(int argc, const char **argv) {
-  auto root = nil::cli::Node::root<Command<0>>();
-  root.add<Command<1>>("hello", "command for 1:hello") //
-      .add<Command<2>>("world", "command for 2:world");
-  root.add<Command<3>>("another", "command for 3:another") //
-      .add<Command<4>>("dimension", "command for 4:dimension");
-  return root.run(argc, argv);
 }

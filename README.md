2cebe2e650dc7d96d98384b5b702ae1527b87db6
Neil Aldea <njaldea@gmail.com>
2024-04-17 19:05:54 -0400

    [misc] added fixes

--- README.md
+++ README.md
@@ -2,10 +2,3 @@
 
 This project will simply serve as testing ground for consuming nil libraries + template for creating new c++ projects.
 
-## To configure
-
-```
-> cd ${BUILD_FOLDER}
-> cmake [SOURCE_FOLDER]                                                      \
->     -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
-#include <nil/gate.hpp>
+// g++ main.cpp -o out.a -std=c++20
+// g++ main.cpp -o out.a -std=c++20 -isystem /home/njaldea/repo/cpp/nil-gate/src/publish
 
-int main(int argc, const char** argv)
+#include "../../nil-gate/src/publish/nil/gate.hpp"
+// #include <nil/gate.hpp>
+
+#include <iostream>
+
+void print(int value)
 {
+    std::cout << value << std::endl;
+}
+
+int main()
+{
+    nil::gate::Core core;
+    auto* e = core.edge(100);
+    core.node(&print, {e});
+
+    core.run();
+
+    e->set_value(200);
+    core.run();
+
     return 0;
 }
